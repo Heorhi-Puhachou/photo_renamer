f34327b2fc614c6439a6a9496c32b774315b9cb6
Heorhi Puhachou <h.puhachou@gmail.com>
2021-11-09 17:05:52 +0300

    Refactor with stream.

--- src/main/java/by/heorhi/puhachou/App.java
+++ src/main/java/by/heorhi/puhachou/App.java
@@ -1,59 +1,55 @@
 package by.heorhi.puhachou;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class App {
 
     private static final String PHOTO_FOLDER = "D:\\1";
 
-    /**
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) {
         File folder = new File(PHOTO_FOLDER);
-        File[] listOfFiles = folder.listFiles();
 
-        for (File listOfFile : listOfFiles) {
-            if (listOfFile.isFile()) {
-                File file = new File(PHOTO_FOLDER + "\\" + listOfFile.getName());
-                try {
-                    Metadata metadata = ImageMetadataReader.readMetadata(file);
+        Arrays.stream(folder.listFiles())
+                .filter(File::isFile)
+                .filter(App::hasExifAndDate)
+                .forEach(App::rename);
+    }
 
-                    ExifSubIFDDirectory exif = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-                    if (exif != null) {
-                        Date date = exif.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss");
-                        dateFormat.setTimeZone(TimeZone.getTimeZone("MSK"));
-
-                        if (date != null) {
-                            // ------------------------------------------------------------------------------------------
-                            File newFile = new File(
-                                    PHOTO_FOLDER + "\\" + dateFormat.format(date) + ".JPG");
-                            // ------------------------------------------------------------------------------------------
-
-                            System.out.println(listOfFile.getName());
-                            if (file.renameTo(newFile)) {
-                                System.out.println("Пасьпяховае перайменаваньне");
-                            } else {
-                                System.out.println("Не атрымалася перайменаваць файл");
-                            }
-                        }
-                    }
-                } catch (ImageProcessingException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
+    public static ExifSubIFDDirectory getExif(File photo) {
+        ExifSubIFDDirectory exif;
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(photo);
+            exif = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+        } catch (ImageProcessingException | IOException e) {
+            exif = null;
         }
+        return exif;
+    }
+
+    public static boolean hasExifAndDate(File photo) {
+        return getExif(photo) != null && getExif(photo).getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL) != null;
+    }
+
+    public static String formatDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("MSK"));
+        return dateFormat.format(date);
+    }
+
+    public static void rename(File oldNamePhoto) {
+        ExifSubIFDDirectory exif = getExif(oldNamePhoto);
+        Date date = exif.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+        File newNamePhoto = new File(PHOTO_FOLDER + "\\" + formatDate(date) + ".JPG");
+        oldNamePhoto.renameTo(newNamePhoto);
     }
 }
